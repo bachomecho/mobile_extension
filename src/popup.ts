23ae2599d0c84998ff42e3b480d785560f862d1c
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2023-11-19 22:24:49 +0200

    focus input element

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,6 @@
 // establish long lived connections? https://stackoverflow.com/questions/13546778/how-to-communicate-between-popup-js-and-background-js-in-chrome-extension
 const inputElement = document.getElementById("filtervalue") as HTMLInputElement;
+inputElement.focus();
 const filterButton = document.getElementById("filterbutton");
 function executeFilter() {
   chrome.tabs.query(
