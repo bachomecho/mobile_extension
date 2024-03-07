5038ad35edb4661e7bf5f2e581f29047537e3f3f
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-03-07 23:40:09 +0200

    chore: remove unnecessary logic from background script

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,4 @@
-let cache = new Array<Map<string, Element[]>>()
-
-chrome.runtime.onMessage.addListener(function(request){
-  console.log("connection is healthy.")
-  console.log("message: ", request.queue)
-  if (cache.length >= 3) {
-    cache = cache.slice(1)
-    cache.push(request.queue)
-  }
-  cache.push(request.queue)
-})
-
-console.log("cache look at it!", cache)
-
-interface CacheInfo {
+interface SearchInfo {
   searchValue: string,
   keywords: string,
   filterAmount: number,
